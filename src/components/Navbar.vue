a18866993b9e49a384902dc1ed92e87850d10472
cismohd <cismohd@hotmail.com>
2019-11-28 22:17:36 +0200

    trying is sucessful

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,9 +19,7 @@
             <li class="nav-item">
               <router-link to="/about" class="nav-link" href="#">About</router-link>
             </li>
-            <li class="nav-item">
-              <router-link to="/about" class="nav-link" href="#">About</router-link>
-            </li>
+            
           
           </ul>
           <form class="form-inline my-2 my-lg-0">
